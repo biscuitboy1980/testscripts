8e312b7b87ca9b0019aaed6e14258f385ab645ac
gguenther <biscuitboy1980@gmail.com>
2015-04-06 16:12:30 -0500

    Signed-off-by: gguenther <biscuitboy1980@gmail.com>

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=[
-	{
-		"script": "GCalculator.py",
-		"icon_resources": [(1, "G.ico")]
-	}
-],
+
+setup(
+	windows = [
+		{
+			"script": "GCalculator.py",
+			"icon_resources": [(1, "g.ico")]
+		}
+	],
 )
